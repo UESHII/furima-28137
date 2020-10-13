5b220f1c0f7d60eb7791fd37607fb781f451479a
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-10-13 22:24:47 +0900

    正規表現の変数への代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   
   with_options presence: true do
     validates :nickname
     validates :email
     validates :password, format: { with: PASSWORD_REGEX, message: "is invalid. Input 6 characters at least, use both of letter and digit."}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :kana_family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
-    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
+    validates :family_name, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :kana_family_name, format: { with: KANA_NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :kana_first_name, format: { with: KANA_NAME_REGEX, message: "is invalid. Input full-width characters."}
     validates :birthday
   end
 end
