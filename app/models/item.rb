e64ed425f19cd9e10341933ffcbfac5c22ecdb64
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-10-14 15:55:54 +0900

    登録できる価格の範囲設定を追加 #2

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,6 +22,9 @@ class Item < ApplicationRecord
       greater_than_or_equal_to: 300,
       less_than: 10000000,
       message: "is invalid. Input a price between 300 and 9999999."
+    }, format: {
+      with: /\A[0-9]+\z/,
+      message: "is invalid. Input half-width numbers."
     }
   end
 end
