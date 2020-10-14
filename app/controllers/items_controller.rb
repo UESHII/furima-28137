550a25115dca285c67888daab426ca4a67d7e361
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-10-14 15:48:25 +0900

    ログインユーザーのみ出品ページへ遷移できる機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
     @items = Item.all
   end
@@ -9,4 +10,13 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
