29412e8d82344385df92acef181b9b4594ec2b68
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-10-15 13:27:29 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,32 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  # has_one :purchase
-  # belongs_to_active_hash :category
-  # belongs_to_active_hash :condition
-  # belongs_to_active_hash :delivery_fee
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :lead_time
-  has_one_attached :item
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :lead_time
 
+  belongs_to :user
+  has_one_attached :image
+  # has_one :purchase
+  
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :delivery_fee_id
-    validates :prefecture_id
-    validates :lead_time_id
-    validates :price
+    validates :category
+    validates :condition
+    validates :delivery_fee
+    validates :prefecture
+    validates :lead_time
     validates :user
-    validates :item, numericality: {
+    validates :image
+    validates :price, numericality: {
       greater_than_or_equal_to: 300,
       less_than: 10000000,
-      message: "is invalid. Input a price between 300 and 9999999."
+      message: "price is invalid. Input a price between 300 and 9999999."
     }, format: {
       with: /\A[0-9]+\z/,
-      message: "is invalid. Input half-width numbers."
+      message: "price is invalid. Input half-width numbers."
     }
   end
 end
