c1f9a525d73b1b2957773eeea411c9fa652c18e6
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-10-22 00:46:00 +0900

    商品購入機能のエラーメッセージ修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -4,14 +4,13 @@ class PurchaseAddress
   attr_accessor :postal_code, :prefecture_id, :city, :street_number, :building_name, :tel_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id
     validates :city
     validates :street_number
-    validates :building_name
-    validates :tel_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input is invalid. Input half-width numbers without hyphen."}
-    # validates :token
   end
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Input numbers include hyphen(-)."}
+  validates :tel_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width numbers without hyphens(-)."}
+  validates :token, presence: {message: "is invalid. Input correct card informations."}
 
   def save
     @purchase = Purchase.create(user_id: user_id, item_id: item_id)
